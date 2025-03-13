a74f5cd2dda09cadd315d1af3c407c86d469f976
Gilles Hoarau <unicolored@gmail.com>
2025-03-13 21:52:30 +0100

    Add player components and update theme integration
    Introduce `PlayerComponent` and `PlayersComponent` for modular player display and interactions. Refactor `live.component` to utilize reusable components and integrate new theme logic, enhancing maintainability and UI consistency. Update `StreamService` logic for streamlined channel loading, improve chessboard camera handling, and refine theme toggle behavior.

--- src/app/pages/page-live/live.component.ts
+++ src/app/pages/page-live/live.component.ts
@@ -2,6 +2,7 @@ import {
   AfterViewInit,
   Component,
   computed,
+  effect,
   ElementRef,
   inject,
   OnDestroy,
@@ -12,70 +13,65 @@ import {
 } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
-import { LichessTvFeed, PlayerUI } from "./feed.interface";
+import { LichessTvFeed } from "./feed.interface";
 import { Subscription } from "rxjs";
 import { Chessfield } from "chessfield";
 import * as cg from "chessground/types";
 import { StreamService } from "../../service/stream.service";
 import { isPlatformBrowser } from "@angular/common";
-import { Player, Players } from "../../board/lichess.interface";
+import { Player } from "../../board/lichess.interface";
+import {
+  BothPlayers,
+  PlayersComponent,
+} from "../../component/players/players.component";
+import { ThemeService } from "../../service/theme.service";
 
 @Component({
   standalone: true,
-  imports: [FormsModule, RouterModule, ReactiveFormsModule],
+  imports: [FormsModule, RouterModule, ReactiveFormsModule, PlayersComponent],
   template: `
-    <!--    <select [(ngModel)]="selectedChannel" (change)="loadChannel()">-->
-    <!--      <option value="top">Top Rated</option>-->
-    <!--      <option value="blitz">Blitz</option>-->
-    <!--      <option value="bullet">Bullet</option>-->
-    <!--    </select>-->
-    <!--    <button (click)="randomFen()">random</button>-->
+    <div class="my-2 flex justify-between">
+      <div class="flex w-1/2 justify-between">
+        <select class="select select-primary">
+          <option value="top">Lichess</option>
+        </select>
 
-    <div class="players-wrap m-auto flex justify-between p-4">
-      @if (playersComputed()) {
-        <div class="badge badge-lg">
-          <span class="mb-2 text-xl">♔</span>
-          <b>{{ playersComputed().white.name }}</b>
+        <select
+          class="select select-primary"
+          [(ngModel)]="selectedChannel"
+          (change)="loadChannel()"
+        >
+          <option disabled selected>Channel</option>
+          <option value="top">Top Rated</option>
+          <option value="bullet">Bullet</option>
+          <option value="blitz">Blitz</option>
+          <option value="rapid">Rapid</option>
+        </select>
+      </div>
 
-          @if (playersComputed().white.title) {
-            <span class="btn btn-ghost px-1">{{
-              playersComputed().white.title
-            }}</span>
-          }
-          <br />
-          {{ playersComputed().white.rating }}
-        </div>
-        <div class="badge badge-lg">
-          <span class="mb-2 text-xl">♚</span>
-          <b>{{ playersComputed().black.name }}</b>
-
-          @if (playersComputed().black.title) {
-            <span class="btn btn-ghost px-1">{{
-              playersComputed().black.title
-            }}</span>
-          }
-          {{ playersComputed().black.rating }}
-        </div>
-      }
+      <!--      <div>-->
+      <!--        <div-->
+      <!--          aria-label="success"-->
+      <!--          class="status status-success status-lg ml-2"-->
+      <!--          [class.animate-ping]="false"-->
+      <!--        ></div>-->
+      <!--        <button class="btn btn-ghost" (click)="togglePause()">-->
+      <!--          {{ isPaused ? "Resume" : "Pause" }}-->
+      <!--        </button>-->
+      <!--      </div>-->
     </div>
 
     <div class="flex w-full justify-center">
       <div class="chessfield-wrap" #chessfield></div>
     </div>
+    <app-players
+      [players]="playersComputed()"
+      (emitCameraCol)="updateCamera($event)"
+    ></app-players>
 
     <!--    <div class="info-controls">-->
     <!--      <div class="controls">-->
-    <!--        <button (click)="togglePause()">-->
-    <!--          {{ isPaused ? "Resume" : "Pause" }}-->
-    <!--        </button>-->
     <!--        <button (click)="toggleTheme()">Toggle Theme</button>-->
-    <!--        <input-->
-    <!--          type="range"-->
-    <!--          min="200"-->
-    <!--          max="800"-->
-    <!--          [(ngModel)]="boardSize"-->
-    <!--          (input)="updateSize()"-->
-    <!--        />-->
     <!--      </div>-->
     <!--    </div>-->
   `,
@@ -120,12 +116,9 @@ export class LiveComponent implements OnInit, AfterViewInit, OnDestroy {
   chessfieldElement = viewChild<ElementRef>("chessfield");
 
   players = signal<Player[] | null>(null);
-  playersComputed = computed<{
-    white: Partial<PlayerUI>;
-    black: Partial<PlayerUI>;
-  }>(() => {
+  playersComputed = computed<BothPlayers>(() => {
     const players = this.players();
-    let result = {
+    let result: BothPlayers = {
       white: {},
       black: {},
     };
@@ -159,8 +152,16 @@ export class LiveComponent implements OnInit, AfterViewInit, OnDestroy {
   previousId = -1;
   chessfield!: Chessfield;
   platformID = inject(PLATFORM_ID);
+  themeService = inject(ThemeService);
 
-  constructor() {}
+  constructor() {
+    effect(() => {
+      const isDarkMode = this.themeService.isDarkMode();
+      if (this.chessfield) {
+        this.chessfield.configUpdate({ mode: isDarkMode ? "dark" : "light" });
+      }
+    });
+  }
 
   ngOnInit() {
     this.loadChannel();
@@ -173,8 +174,10 @@ export class LiveComponent implements OnInit, AfterViewInit, OnDestroy {
         this.chessfieldElement()?.nativeElement;
 
       this.chessfield = new Chessfield(element, {
-        // orientation: 'white',
-        // camera: 'right',
+        mode: this.themeService.isDarkMode() ? "dark" : "light",
+        theme: "green",
+        camera: "white",
+        angle: "right",
       });
     }
   }
@@ -182,37 +185,48 @@ export class LiveComponent implements OnInit, AfterViewInit, OnDestroy {
   loadChannel() {
     if (this.isPaused) return;
 
+    this.resetFeed();
+
+    const feedsMap = new Map();
+    feedsMap.set("top", "https://lichess.org/api/tv/feed");
+    feedsMap.set("blitz", "https://lichess.org/api/tv/blitz/feed");
+    feedsMap.set("bullet", "https://lichess.org/api/tv/bullet/feed");
+    feedsMap.set("rapid", "https://lichess.org/api/tv/rapid/feed");
+
+    const channelSelected = feedsMap.get(this.selectedChannel);
+
     // const stream = fetch('https://lichess.org/api/games/user/neio',{headers:{Accept:'application/x-ndjson'}});
-    this.subscription = this.streamService
-      .startTv("https://lichess.org/api/tv/feed")
-      .subscribe({
-        next: (data: LichessTvFeed | null) => {
-          if (data) {
-            if (data.t === "featured") {
-              const players = data.d.players;
-              if (players) {
-                this.players.set(players);
-              }
-            }
-            if (data.t === "fen") {
-              const lastMove = data.d.lm?.match(/.{1,2}/g) as cg.Key[];
-              this.chessfield.setFen(data.d.fen, lastMove);
+    this.subscription = this.streamService.startTv(channelSelected).subscribe({
+      next: (data: LichessTvFeed | null) => {
+        if (data) {
+          if (data.t === "featured") {
+            const players = data.d.players;
+            if (players) {
+              this.players.set(players);
             }
           }
-        },
-        error: (err) => {
-          console.error("Stream error:", err);
-        },
-        complete: () => {
-          console.log("Stream completed");
-        },
-      });
+          if (data.t === "fen") {
+            const lastMove = data.d.lm?.match(/.{1,2}/g) as cg.Key[];
+            this.chessfield.setFen(data.d.fen, lastMove);
+          }
+        }
+      },
+      error: (err) => {
+        console.error("Stream error:", err);
+      },
+      complete: () => {
+        console.log("Stream completed");
+      },
+    });
   }
 
   togglePause() {
     // this.isPaused = !this.isPaused;
-    if (!this.isPaused) this.loadChannel();
-    this.streamService.stopTv();
+    if (!this.isPaused) {
+      this.loadChannel();
+    } else {
+      this.streamService.stopTv();
+    }
   }
 
   toggleTheme() {
@@ -224,11 +238,15 @@ export class LiveComponent implements OnInit, AfterViewInit, OnDestroy {
     // this.chessfield.updatePosition(this.chessfield['fen'], { size: this.boardSize });
   }
 
-  ngOnDestroy(): void {
+  resetFeed(): void {
     if (this.subscription) {
       this.subscription.unsubscribe();
     }
     this.streamService.stopTv();
+  }
+
+  ngOnDestroy(): void {
+    this.resetFeed();
   }
 
   randomFen() {
@@ -241,5 +259,10 @@ export class LiveComponent implements OnInit, AfterViewInit, OnDestroy {
 
     const { fen, lastMove } = this.randomFens[id];
     this.chessfield.setFen(fen, lastMove);
+  }
+
+  updateCamera(event: string | undefined) {
+    // @ts-ignore
+    this.chessfield.configUpdate({ camera: event as Camera, angle: "center" });
   }
 }
