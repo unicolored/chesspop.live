341439dab9ec0afc62ce08b6634fe48e140ab86f
Gilles Hoarau <unicolored@gmail.com>
2025-03-17 22:11:14 +0100

    Add About page with component, routing, and styling
    Introduce an "About" page to provide information about the app, including motivations, features, and contact/support details. Added routing, HTML, CSS, and unit tests for the component. Also updated navigation and enhanced global styles with Tailwind's typography plugin. Removed a debug log from the audio service.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,4 +7,10 @@ export const routes: Routes = [
     loadComponent: () =>
       import("./pages/page-live/live.component").then((m) => m.LiveComponent),
   },
+  {
+    path: "about",
+    pathMatch: "full",
+    loadComponent: () =>
+      import("./pages/about/about.component").then((m) => m.AboutComponent),
+  },
 ];
