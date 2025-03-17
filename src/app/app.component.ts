183f20e68a60015f681c825fbd2a21193da8d4fa
Gilles Hoarau <unicolored@gmail.com>
2025-03-17 13:24:32 +0100

    Update navbar with new branding and FontAwesome icons
    Replaced placeholder navbar content with new links and branding, including a "ChessPop" logo and a "Live" link. Integrated FontAwesome for the icons and updated component imports accordingly.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { ThemeService } from "./service/theme.service";
 import { ThemeToggleComponent } from "./component/theme-toggle/theme-toggle.component";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { faCoffee } from "@fortawesome/sharp-duotone-solid-svg-icons";
 
 @Component({
   selector: "app-root",
-  imports: [RouterOutlet, ThemeToggleComponent],
+  imports: [RouterOutlet, ThemeToggleComponent, FontAwesomeModule],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css",
   providers: [ThemeService],
