e77ca0ff2bd16e95a2cea49846ce345240f0cc54
Gilles Hoarau <unicolored@gmail.com>
2025-03-24 20:47:55 +0100

    Standardize quotes and fix minor formatting in tests
    Replaced single quotes with double quotes in

--- src/app/pages/widget/widget.component.spec.ts
+++ src/app/pages/widget/widget.component.spec.ts
@@ -1,23 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { WidgetComponent } from './widget.component';
+import { WidgetComponent } from "./widget.component";
 
-describe('WidgetComponent', () => {
+describe("WidgetComponent", () => {
   let component: WidgetComponent;
   let fixture: ComponentFixture<WidgetComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [WidgetComponent]
-    })
-    .compileComponents();
+      imports: [WidgetComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(WidgetComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
