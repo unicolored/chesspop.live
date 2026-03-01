9bc3b183b3437f0c21c49eba577468a28dcb87c3
Gilles Hoarau <unicolored@gmail.com>
2026-03-01 21:26:20 +0100

    yarn format

--- src/app/component/players/player/player.component.ts
+++ src/app/component/players/player/player.component.ts
@@ -7,8 +7,16 @@ import { PlayerUI } from "../../../pages/page-live/feed.interface";
   template: `
     <div class="app-player">
       <div class="badge">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-500 dark:text-gray-500" viewBox="0 0 24 24" fill="currentColor" [title]="p().color === 'white' ? 'White' : 'Black'">
-          <path d="M12 2a2 2 0 0 0-2 2v3H8a2 2 0 0 0-2 2v1H4v2h2v7a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-7h2v-2h-2V9a2 2 0 0 0-2-2h-2V4a2 2 0 0 0-2-2zm-2 7v3h-2V9h2zm4 0v3h-2V9h2z"/>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-5 w-5 text-gray-500 dark:text-gray-500"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          [title]="p().color === 'white' ? 'White' : 'Black'"
+        >
+          <path
+            d="M12 2a2 2 0 0 0-2 2v3H8a2 2 0 0 0-2 2v1H4v2h2v7a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-7h2v-2h-2V9a2 2 0 0 0-2-2h-2V4a2 2 0 0 0-2-2zm-2 7v3h-2V9h2zm4 0v3h-2V9h2z"
+          />
         </svg>
 
         @if (p().title) {
